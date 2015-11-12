0dfe6885bd233350cf6a680ea1353455e6606bc3
Frederic Lavigne <fred@L2FProd.com>
2015-11-12 13:05:57 +0100

    initial checkin

---
+++ app.js
@@ -0,0 +1,29 @@
+// Licensed under the Apache License. See footer for details.
+var
+	express = require('express'),
+	app = express(),
+	cfenv = require('cfenv');
+
+// load local VCAP configuration
+var vcapLocal = null
+try {
+  vcapLocal = require("./vcap-local.json");
+  console.log("Loaded local VCAP", vcapLocal);
+} catch (e) {
+  console.error(e);
+}
+
+// get the app environment from Cloud Foundry, defaulting to local VCAP
+var appEnvOpts = vcapLocal ? {
+  vcap: vcapLocal
+} : {}
+var appEnv = cfenv.getAppEnv(appEnvOpts);
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, "0.0.0.0", function () {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});

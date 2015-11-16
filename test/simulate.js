5aaa1a920a48980815cf381fc94fd4e8882908b4
Frank Wagner <frank.wagner@fr.ibm.com>
2015-11-16 18:55:11 +0100

    add script to simulate scenarios

---
+++ test/simulate.js
@@ -0,0 +1,41 @@
+/*
+  USAGE
+  node test/simulate.js <ID> <TITLE> [<URL>]
+*/
+
+var request = require("request");
+
+var id = process.argv[2]
+var title = process.argv[3]
+var url = process.argv[4]
+
+var base0 = "http://localhost:6001"
+var base = url ? url : base0
+
+var getScenarioUrl = function(id,title) {
+  return base + "/api/1/scenario.json"
+  + (id?"?id="+id+"&title="+title:"")
+}
+
+var getMonitorUrl = function() {
+  return base + "/api/1/monitor.json"
+}
+
+console.log("Simulate scenario",id,title)
+request(getScenarioUrl(id,title), function(error, response, body) {
+  if ( error ) {
+    var status = response ? response["statusCode"] : "no response"
+    console.log("Scenario returns",status,"error: ",error)
+    return
+  }
+  console.log("Scenarios",JSON.parse(body))
+})
+
+request(getMonitorUrl(), function(error, response, body) {
+  if ( error ) {
+    var status = response ? response["statusCode"] : "no response"
+    console.log("Monitor returns",status,"error: ",error)
+    return
+  }
+  console.log("Monitor",JSON.parse(body))
+})
